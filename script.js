ddd63ae70b50b8ef7995698fa53994268a9f895f
emilymacleod <margaretemacleod@gmail.com>
2014-10-03 09:46:31 -0300

    added getElementById
    added getElementById, added sendMessage to get message to come up on
    the screen, cleaned up HTML

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
  function sendMessage(event){
     var msg = document.getElementById("msg-form").value;
     var text = "";
-        text += x.text-box.value + "<br>";
+    document.getElementById("TextArea").innerHTML = msg;
+    //    text += document.getElementById.value + "<br>";
+    
     };
 
-    document.getElementById("msg-form").value =""
+    //     document.getElementById("msg-form").value ="";
 
  function writeSomething(){
+ 	sendMessage("asd");
  	alert ("Welcome to my chat!");
- }
+ 	};
