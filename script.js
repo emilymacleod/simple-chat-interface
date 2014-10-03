79a8bbfe0bc5cff1fd14a7c33afb7cd521ac9ae6
emilymacleod <margaretemacleod@gmail.com>
2014-10-03 11:30:26 -0300

    enter name in text area "said"

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
+ 
+	// this is how i append messages to my chat window
+    //    text += document.getElementById.value + "<br>";
+ var person = '';
+
  function sendMessage(event){
     var msg = document.getElementById("msg-form").value;
     var text = "";
     var parag = document.createElement("P");
-	var stuff = document.createTextNode(msg);
+	var stuff = document.createTextNode(person+" said: "+msg);
 	parag.appendChild(stuff);
 	document.getElementById("TextArea").appendChild(parag);
-    //    text += document.getElementById.value + "<br>";
 	};
 
     //     document.getElementById("msg-form").value ="";
+   // this is how messages are sent to my chat window
 
  function writeSomething(){
  	sendMessage("ads");
  	alert ("Welcome to my chat!");
  	};
 
+ 	// this is how my enter name button function was made
+
  function enterName() {
-    var person = prompt("Please enter your name", "");
+     person = prompt("Please enter your name ", "");
     
     if (person != null) {
         document.getElementById("name").innerHTML =
