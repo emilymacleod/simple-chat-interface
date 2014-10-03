6ed53d75984e5b1c9ef49a13d3c43f0082735bb2
emilymacleod <margaretemacleod@gmail.com>
2014-10-03 09:59:49 -0300

    added comments
    deleted var node

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
  function sendMessage(event){
     var msg = document.getElementById("msg-form").value;
     var text = "";
+    var node =document.getElementById("msg-form")
     document.getElementById("TextArea").innerHTML = msg;
     //    text += document.getElementById.value + "<br>";
-    
-    };
+	};
 
     //     document.getElementById("msg-form").value ="";
 
