90a72290fa3f07bc2e554dfcf4a386ab65c6deed
emilymacleod <margaretemacleod@gmail.com>
2014-10-03 10:49:57 -0300

    added greeting button

--- script.js
+++ script.js
@@ -14,3 +14,13 @@
  	sendMessage("ads");
  	alert ("Welcome to my chat!");
  	};
+
+ function enterName() {
+    var person = prompt("Please enter your name", "");
+    
+    if (person != null) {
+        document.getElementById("name").innerHTML =
+        "Hello " + person + "! How are you today?";
+
+ 		};
+ 	}; 
