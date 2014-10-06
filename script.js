5a6021ef409f9cfeff01ec82b23ce0b15aa5610a
emilymacleod <margaretemacleod@gmail.com>
2014-10-06 11:44:53 -0300

    removes text after submit

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
- 
-	// this is how i append messages to my chat window
-    //    text += document.getElementById.value + "<br>";
- var person = "";
+var person = "";
 
  function sendMessage(event){
     var msg = document.getElementById("msg-form").value;
     var parag = document.createElement("P");
-	var stuff = document.createTextNode(person+" said: "+msg);
-	parag.appendChild(stuff);
-	document.getElementById("TextArea").appendChild(parag);
-	};
+var stuff = document.createTextNode(person+" said: "+msg);
+parag.appendChild(stuff);
+document.getElementById("TextArea").appendChild(parag);
+document.getElementById('msg-form').value='';
+  };
 
     //     document.getElementById("msg-form").value ="";
    // this is how messages are sent to my chat window
 
   function writeSomething(){
- 	 sendMessage("ads");
- 	 alert ("Welcome to my chat!");
- 	 };
+   sendMessage("ads");
+   };
 
- 	// this is how my enter name button function was made
+  // this is how my enter name button function was made
 
  function enterName() {
      person = prompt("Please enter your name ", "");
@@ -27,6 +24,5 @@
     if (person != null) {
         document.getElementById("name").innerHTML =
         "Hello " + person + "! How are you today?";
-
- 		};
- 	}; 
+  };
+};
